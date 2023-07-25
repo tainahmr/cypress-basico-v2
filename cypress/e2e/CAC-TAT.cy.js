79b85c9a102769580961bdad6e31e53de5c46e7f
tainahmr <tainahmr@gmail.com>
2023-07-25 14:39:56 -0300

    delete

--- cypress/e2e/CAC-TAT.cy.js
+++
@@ -1,8 +0,0 @@
-/*<reference types="Cypress" />*/ 
-
-describe('Central de Atendimento ao Cliente TAT', function() {
-  it('verifica o título da aplicação', function() {
-    cy.visit('./src/index.html')
-    cy.title().should('be.should', 'Central de Atendimento ao Cliente TAT'),
-  })
-})
